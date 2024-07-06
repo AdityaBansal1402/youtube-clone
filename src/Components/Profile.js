534ed6c838549555c10552cac31607d17496f950
AdityaBansal1402 <bansaladitya3009@gmail.com>
2024-07-06 23:55:13 +0530

    uploading..

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -6,7 +6,7 @@ const Profile = () => {
     <div className='bg-gray-950 min-h-screen'>
       {(localStorage.getItem('token'))?<div className='flex flex-col'>
         <div className='pt-20 pl-32 border-b-4 border-gray-600 sticky top-0 bg-gray-950'>
-            <h1 className='text-white text-xl pb-6 font-semibold font-mono'>Hello Aditya</h1>
+            <h1 className='text-white text-xl pb-6 font-semibold font-mono'>Hello </h1>
             <ul className="flex flex-wrap -mb-px">
                 <li className="me-2">
                     <NavLink to='about'  className="text-gray-400 inline-block p-4 border-b-4 border-transparent rounded-t-lg hover:text-gray-600 hover:border-white dark:hover:text-white">Profile</NavLink>
