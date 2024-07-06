65d05c97d180d419f6cfe6e1d6625cac5b0f25a8
AdityaBansal1402 <bansaladitya3009@gmail.com>
2024-07-06 23:58:15 +0530

    Change names in Profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, NavLink,Outlet } from 'react-router-dom';
+import InfoContext from '../Context/Info/InfoContext';
 
 const Profile = () => {
+  const usercontext = useContext(InfoContext);
+  const { user, getuser } = usercontext;
   return (
     <div className='bg-gray-950 min-h-screen'>
       {(localStorage.getItem('token'))?<div className='flex flex-col'>
         <div className='pt-20 pl-32 border-b-4 border-gray-600 sticky top-0 bg-gray-950'>
-            <h1 className='text-white text-xl pb-6 font-semibold font-mono'>Hello </h1>
+            <h1 className='text-white text-xl pb-6 font-semibold font-mono'>Hello {user.name.split(' ')[0]}</h1>
             <ul className="flex flex-wrap -mb-px">
                 <li className="me-2">
                     <NavLink to='about'  className="text-gray-400 inline-block p-4 border-b-4 border-transparent rounded-t-lg hover:text-gray-600 hover:border-white dark:hover:text-white">Profile</NavLink>
--- src/Components/organism/About.js
+++ src/Components/organism/About.js
@@ -1,8 +1,12 @@
 import React, { useContext, useEffect, useMemo } from 'react'
 import VideoContext from '../../Context/video/Videocontext';
+import InfoContext from '../../Context/Info/InfoContext';
+
 
 const About = () => {
   const context = useContext(VideoContext);
+  const usercontext = useContext(InfoContext);
+  const { user, getuser } = usercontext;
   const { vids, getvids } = context;
 
   useMemo(()=>{
@@ -17,7 +21,7 @@ const About = () => {
             <div key={vid.id} className='flex flex-col border-0 border-gray-600 grow basis-1/4 h-fit max-w-min min-w-80'>
                <div className='h-fit'><img className='rounded-xl max-h-44' src={vid.imgurl} alt="Girl in a jacket"/></div>
                <div className='pl-2 pt-2 font-bold'>{vid.title}</div>
-               <div className='pl-2 text-sm'>Username</div>
+               <div className='pl-2 text-sm'>{user.name.split(' ')[0]}</div>
                <div className='pl-2 flex'><div className='basis-1/2'>Views: {vid.views}</div></div>
              </div>)
         })}
